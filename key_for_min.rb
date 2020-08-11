7689bd33579eae2c1bf873545f34e57815c3345a
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-11 18:54:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,11 @@
 def key_for_min_value(name_hash)
   min_key=nil
   min_value=0
-  name_hash.each do |name, value|
+  name_hash.each do |key, value|
     if min_value==0 ||
       value<min_value
       min_value=value 
-  else min_value==nil
+      min_key=key
     end
   end
 key_for_min_value(name_hash)
